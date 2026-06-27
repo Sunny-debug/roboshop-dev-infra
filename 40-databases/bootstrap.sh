3c67b67f82707f26ea060dead6998d2027604c9f
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2026-06-27 15:11:39 -0500

    Redis config added

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+server_name=$1
 dnf install ansible -y 
-ansible-pull -U https://github.com/Sunny-debug/ansible-roboshop-roles.git -e server_name=mongodb main.yaml 
+ansible-pull -U https://github.com/Sunny-debug/ansible-roboshop-roles.git -e server_name=$server_name main.yaml 
