bc801c9fcdf16245edd0550297530f1162a1f92e
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2026-06-27 11:48:06 -0500

    Databases added

---
+++ 40-databases/locals.tf
@@ -0,0 +1,11 @@
+locals {
+  common_name_suffix = "${var.project_name}-${var.environment}"
+  ami_id = data.aws_ami.dev-ami.id
+  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+  database_subnet_id = split("," ,data.aws_ssm_parameter.database_subnet_id.value)[0]
+  common_tags = {
+    project     = var.project_name
+    environment = var.environment
+    terraform   = "true"
+  }  
+}
