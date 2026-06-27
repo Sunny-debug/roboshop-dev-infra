09c8c47e74d558365d3fa2e4e7fc0cc011faae3b
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2026-06-27 15:17:43 -0500

    Redis SG config added

--- 30-sg-rules/data.tf
+++ 30-sg-rules/data.tf
@@ -11,5 +11,5 @@ data "aws_ssm_parameter" "mongodb_sg_id" {
 }
 
 data "aws_ssm_parameter" "redis_sg_id" {
-  name = "/${var.project_name}/${var.environment}/redsis_sg_id"
+  name = "/${var.project_name}/${var.environment}/redis_sg_id"
 }
