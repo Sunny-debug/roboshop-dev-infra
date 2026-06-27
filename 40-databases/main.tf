6e1d5e63ebc41722d5b259769c15044f0c4664a3
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2026-06-27 15:19:43 -0500

    Redis SG config added

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -37,7 +37,7 @@ resource "terraform_data" "mongodb" {
     }
 }
 
-resource "aws_instance" "mongodb" {
+resource "aws_instance" "redis" {
     ami = local.ami_id
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.redis_sg_id]
