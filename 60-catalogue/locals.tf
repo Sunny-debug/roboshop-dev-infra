34904d94fb116c753859e8f4306daa16c44b08f0
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2026-07-02 21:32:02 -0500

    Catalogue Steps Added

---
+++ 60-catalogue/locals.tf
@@ -0,0 +1,11 @@
+locals {
+  common_name_suffix = "${var.project_name}-${var.environment}"
+  ami_id = data.aws_ami.dev-ami.id
+  catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+  private_subnet_id = split("," ,data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  common_tags = {
+    project     = var.project_name
+    environment = var.environment
+    terraform   = "true"
+  }
+}
