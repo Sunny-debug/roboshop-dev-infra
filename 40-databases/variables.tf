4a0a7b605ef479a68bfd28f0fed893fb27dbd474
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2026-07-02 21:19:35 -0500

    Route53 records Added

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -5,3 +5,12 @@ variable "project_name" {
 variable "environment" {
   default = "dev"
 }
+
+variable "zone_id" {
+  default = "Z0095538Q74XD6XU6M0Z"
+}
+
+variable "domain_name" {
+  default = "dawgs.online"
+  
+}
